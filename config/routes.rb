3b18a0028d0e07e478ad67786c092a3f3d73aa54
Max Dignan <maxdignan@gmail.com>
2022-01-26 22:41:47 -0500

    chore: adding quoting route

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resources :customers
   resources :businesses
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "/invoices-quote", to: "invoices#quote"
 end
